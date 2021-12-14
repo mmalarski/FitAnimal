2f812947418202d0713972371ebb0340416d6081
mmalarski <malarsky.mike@gmail.com>
2021-12-14 14:17:03 +0100

    left screen and textfield added

--- app/src/main/java/com/example/fitanimal/MainActivity.kt
+++ app/src/main/java/com/example/fitanimal/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.fitanimal
 
+import android.animation.ValueAnimator
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "com.example.fitanimal.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun sendMessage(view: View) {
+
+        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
     }
 }
