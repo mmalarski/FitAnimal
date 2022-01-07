5fc5adda542eb68a396f29c3268d0d5a2b14daf0
mmalarski <malarsky.mike@gmail.com>
2022-01-07 18:14:43 +0100

    qr generation done

--- app/src/main/java/com/example/fitanimal/SettingActivity.kt
+++ app/src/main/java/com/example/fitanimal/SettingActivity.kt
@@ -5,11 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 
 class SettingActivity : AppCompatActivity() {
+
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
+        textView = findViewById(R.id.textViewChangeUsername)
     }
 
     fun goToSharing(view: View) {
@@ -18,4 +23,5 @@ class SettingActivity : AppCompatActivity() {
         startActivity(intent);
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
     }
+
 }
--- app/src/main/java/com/example/fitanimal/SharingActivity.kt
+++ app/src/main/java/com/example/fitanimal/SharingActivity.kt
@@ -2,31 +2,62 @@ package com.example.fitanimal
 
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Layout
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.budiyev.android.codescanner.*
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.common.BitMatrix
+import com.journeyapps.barcodescanner.BarcodeEncoder
+import java.lang.Exception
 import java.util.*
 
 private const val CAMERA_REQUEST_CODE = 101
 
 class SharingActivity : AppCompatActivity() {
 
+    private lateinit var bitMatrix: BitMatrix
+    private lateinit var bitMap: Bitmap
     private lateinit var codeScanner: CodeScanner
     private lateinit var textView: TextView
     private lateinit var scanner_view: CodeScannerView
+    private lateinit var imageView: ImageView
+    private lateinit var multiFormatWriter: MultiFormatWriter
+    private lateinit var barcodeEncoder: BarcodeEncoder
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val text = "sample text"
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sharing)
         textView = findViewById<TextView>(R.id.textView2)
         scanner_view = findViewById<CodeScannerView>(R.id.scanner_view)
+        imageView = findViewById<ImageView>(R.id.imageViewSharing)
+
+        val multiFormatWriter = MultiFormatWriter()
+        try {
+            val bitMatrix = multiFormatWriter.encode(
+                text,
+                BarcodeFormat.QR_CODE,
+                500,
+                500
+            )
+            val barcodeEncoder = BarcodeEncoder()
+            val bitmap = barcodeEncoder.createBitmap(bitMatrix)
+            imageView.setImageBitmap(bitmap)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
 
         setupPermission()
         codeScanner()
