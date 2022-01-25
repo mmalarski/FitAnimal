bd75b5999da7b97eec1c2812f6fee9397d4fa6f8
229895 <229895@edu.p.lodz.pl>
2022-01-25 13:14:42 +0100

    increased test coverage in SettingActivity to 100%

--- app/src/androidTest/java/com/example/fitanimal/SettingActivityTest.kt
+++ app/src/androidTest/java/com/example/fitanimal/SettingActivityTest.kt
@@ -1,34 +1,25 @@
 package com.example.fitanimal
 
 import android.view.View
-import androidx.lifecycle.Lifecycle
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.intent.Intents.intended
-import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
-import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.ext.junit.rules.activityScenarioRule
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
 import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
 
 class SettingActivityTest {
 
 
-    @Rule(order = 0)
+    @Rule
     @JvmField
     val mActivityTestRule = ActivityTestRule(SettingActivity::class.java)
-
-    @get:Rule(order = 1)
-    val intentRule = IntentsTestRule(SharingActivity::class.java)
 
     private var mActivity : SettingActivity? = null
 
@@ -43,12 +34,11 @@ class SettingActivityTest {
         assertNotNull(view)
     }
 
-    //TODO(cannot find button4, figure out why)
-//    @Test
-//    fun goToSharingTest() {
-//        onView(withId(R.id.button4)).perform(click()) //to nie git
-//        intended(hasComponent(SharingActivity::class.java.name)) //to git
-//    }
+    @Test
+    fun goToSharingTest() {
+        onView(withId(R.id.button4)).perform(click())
+        onView(withId(R.id.scanner_view)).check(matches(isDisplayed()))
+    }
 
     @After
     fun tearDown() {
