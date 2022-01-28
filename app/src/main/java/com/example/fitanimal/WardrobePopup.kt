341c42d107c7dbd20e1db4565808c5973545d858
mmalarski <malarsky.mike@gmail.com>
2022-01-28 02:14:06 +0100

    wardrobe: padlocks added

--- app/src/main/java/com/example/fitanimal/WardrobePopup.kt
+++ app/src/main/java/com/example/fitanimal/WardrobePopup.kt
@@ -3,6 +3,9 @@ package com.example.fitanimal
 import android.app.Activity
 import android.os.Bundle
 import android.util.DisplayMetrics
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
 
 
 class WardrobePopup : Activity() {
@@ -16,5 +19,33 @@ class WardrobePopup : Activity() {
         val height = dm.heightPixels
 
         window.setLayout((width * 0.9).toInt(), (height * 0.9).toInt())
+        findViewById<ImageView>(R.id.padlock).setOnLongClickListener {
+            it.visibility = View.GONE
+            true
+        }
+        findViewById<ImageView>(R.id.padlock2).setOnLongClickListener {
+            it.visibility = View.GONE
+            true
+        }
+        findViewById<ImageView>(R.id.padlock3).setOnLongClickListener {
+            it.visibility = View.GONE
+            true
+        }
+        findViewById<ImageView>(R.id.padlock4).setOnLongClickListener {
+            it.visibility = View.GONE
+            true
+        }
+        findViewById<ImageView>(R.id.padlock5).setOnLongClickListener {
+            it.visibility = View.GONE
+            true
+        }
+        findViewById<ImageView>(R.id.padlock6).setOnLongClickListener {
+            it.visibility = View.GONE
+            true
+        }
+    }
+
+    fun displayHint(view: View) {
+        Toast.makeText(this,"Long tap to purchase - 1000 coins", Toast.LENGTH_LONG).show()
     }
 }
